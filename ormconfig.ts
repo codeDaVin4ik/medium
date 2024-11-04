6fc6a410f0ee719f301ba7e28bdc7aa71df16978
Ivan Kyselov <kyselovivan@gmail.com>
2024-11-04 18:00:56 +0200

    feature/added_migrratioins_drop/create/generate

---
+++ ormconfig.ts
@@ -0,0 +1,15 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+
+const config = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'ivank',
+    password: '123',
+    database: 'lastMedium',
+    entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    migrations: [__dirname + '/src/migrations/**/*{.ts,.js}']
+}
+
+export default config;
