627b48f12d72b807a669d65e1854e0bfe77e2667
Ivan Kyselov <kyselovivan@gmail.com>
2024-11-04 14:15:41 +0200

    config/create_absolut_path

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TagService{
     fiindAll(){
-        return ['coffee', 'dragons', 'service'];
+        return ['job','coffee'];
     }
 }
