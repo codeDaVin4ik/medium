b571b8bda73dc004b59c7090b744d42037f5137a
Ivan Kyselov <kyselovivan@gmail.com>
2024-12-17 12:26:33 +0200

    article creation implemented

--- src/articles/article.controller.ts
+++ src/articles/article.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ArticleService } from "./articleService.service";
+import { AuthGuard } from "@app/user/guards/userGuard";
+import { UserEntity } from "@app/user/user.entity";
+import { CreateArticleDto } from "./dto/CreateArticleDto";
+import { User } from "@app/user/decorators/userDecorator";
 
 @Controller('/articles')
 export class ArticleController{
@@ -9,5 +13,11 @@ export class ArticleController{
     @Get()
     getAll(){
         return this.articleService.getAll();
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async create(@User() currentUser: UserEntity, @Body('article') createArticleDto: CreateArticleDto): Promise<any>{
+        return await this.articleService.createArticle(currentUser, createArticleDto);
     }
 }
--- src/articles/articleService.service.ts
+++ src/articles/articleService.service.ts
@@ -1,7 +1,25 @@
+import { UserEntity } from "@app/user/user.entity";
 import { Injectable } from "@nestjs/common";
+import { CreateArticleDto } from "./dto/CreateArticleDto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ArticleEntity } from "./article.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class ArticleService {
+
+    constructor(@InjectRepository(ArticleEntity) private readonly articleRepository: Repository<ArticleEntity>){}
+
+    async createArticle(currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<any> {
+        const article = new ArticleEntity();
+        Object.assign(article, createArticleDto);
+        if(!article.taglist){
+            article.taglist = [];
+        }
+        article.slug = 'foo'
+        article.author = currentUser;
+        return await this.articleRepository.save(article)
+    }
     getAll(){
         return 'all articles';
     }
